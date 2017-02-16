cfabf0fba74b48c84bcdb3ac756f8e3d8817a61d
Swann31 <manauskum@hotmail.com>
2017-02-16 12:14:32 +0100

    Création des différentes interfaces nécessaires à la lecture du fichier CSV et des différentes classes liées

---
+++ GestionnaireDeMission/src/gestionnairedemission/CsvEmployeDAO.java
@@ -0,0 +1,26 @@
+package gestionnairedemission;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CsvEmployeDAO implements EmployeDAO {
+
+    private File f;
+    private CsvFile csvF;
+    // Constructeur
+    private CsvEmployeDAO() {
+        super();
+    }
+    public CsvEmployeDAO(File file) {
+        this.();
+        this.f=file;
+        this.csvF = new CsvFile; 
+    }
+    @Override
+    public List<Employe> addEmploye() {
+        List<Employe> listE = new ArrayList<>();
+        return listE;
+    }
+    
+}
