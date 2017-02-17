4f7144a6d2e27615eb3d5a7c7b9190672986c5c1
Swann31 <manauskum@hotmail.com>
2017-02-17 10:51:15 +0100

    Ajout des commentaires sur les différentes classes crées

--- GestionnaireDeMission/src/gestionnairedemission/CsvEmployeDAO.java
+++ GestionnaireDeMission/src/gestionnairedemission/CsvEmployeDAO.java
@@ -1,8 +1,15 @@
+/*
+classe EmployeDAO correspondant à l'interface liée au Data Acces Object, évitant d'accéder directement aux données brutes de l'objet concerné, ici Employe
+*/
 package gestionnairedemission;
 
 import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class CsvEmployeDAO implements EmployeDAO {
 
@@ -19,8 +26,32 @@ public class CsvEmployeDAO implements EmployeDAO {
     }
     @Override
     public List<Employe> addEmploye() {
-        List<Employe> listE = new ArrayList<>();
+        final List<Employe> listE = new ArrayList<>();
+        List<String[]> data;
+        data = this.csvF.getData();
+        final List<String[]> dataWT = data;
+        dataWT.remove(0);
+        for(String[] data1 : dataWT)
+        {
+            final Employe employe;
+            employe = employeTab(data1);
+            listE.add(employe);
+        }   
         return listE;
+    }
+    private Employe employeTab(String[] tab)
+    {
+        Employe e = new Employe();
+        e.setIdE(Integer.parseInt(tab[3]));
+        e.setPrenom(tab[0]);
+        e.setNom(tab[1]);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+        try {
+            e.setDateEntree(sdf.parse(tab[2]));
+        } catch (ParseException ex) {
+            Logger.getLogger(CsvEmployeDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return e;
     }
     
 }
